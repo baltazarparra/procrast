83a668ff4c4f91f31cc3845686dc58c43444c8dd
baltz <baltazar.parra@fliper.app>
2019-09-28 12:59:59 -0300

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import React, { useContext } from 'react'
 import { RoContext } from './Store'
+import styled from 'styled-components'
+import { Reset } from 'styled-reset'
+
+const List = styled.ul`
+  padding: 1em;
+  background: #f4f4f4;
+`
+
+const ListItem = styled.li`
+  margin: 1em;
+`
+
+const Link = styled.a`
+  display: block;
+  font-family: sans-serif;
+  text-decoration: none;
+  color: #333;
+`
+
+const Sub = styled.p`
+  display: inline-block;
+  font-family: sans-serif;
+  font-size: .8em;
+  color: #666;
+  padding-right: .4em;
+
+  a {
+    color: #666;
+  }
+`
 
 export default function App() {
   const [roPost] = useContext(RoContext)
   return (
     <>
-      <ul>
-        {roPost.map(post => <li key={post.id}>
-          <a href={post.url}>{post.title}</a>
-          <span>{post.author}</span>
-          <i>Score: {post.score}</i>
-          <a href={post.comment_link}>
-            <b>{post.comments}</b> comments
-          </a>
-        </li>)}
-      </ul>
+      <List>
+        <Reset />
+        {roPost.map(post => <ListItem key={post.id}>
+          <Link href={post.url}>{post.title}</Link>
+          { post.author &&
+            <Sub>{post.author}</Sub>
+          }
+          { post.score && <Sub>Score: {post.score}</Sub> }
+          { post.comments > 0 && 
+            <Sub>
+              <a href={post.comment_link}>
+                <b>{post.comments}</b> comments
+              </a>
+            </Sub>
+          }
+        </ListItem>)}
+      </List>
     </>
   )
 }
