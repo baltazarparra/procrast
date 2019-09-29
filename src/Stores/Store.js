e103624b9388efbe992cdf6fca879a93d9d70116
baltz <baltazar.parra@fliper.app>
2019-09-29 02:27:13 -0300

    Add preload

--- src/Stores/Store.js
+++ src/Stores/Store.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react'
+import { Lines } from 'react-preloaders'
 
 export const PostsContext = React.createContext([])
 
 export default function Store({children}) {
     const [post, setPost] = useState([])
+    const [loading, setLoading] = useState(true)
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
         async function fetchData() {
             const response = await fetch('https://www.reader.one/api/all/hn,reddit,ph,slashdot,dn,github,medium,lifehacker?limit=5')
             const data = await response.json()
-            
+
+            setLoading(false)
             setPost(data)
         }
         
@@ -20,6 +23,7 @@ export default function Store({children}) {
     return (
         <PostsContext.Provider value={[post, setPost]}>
             {children}
+            <Lines color={'#bbb'} background="#333" customLoading={loading} />
         </PostsContext.Provider>
     )
 }
